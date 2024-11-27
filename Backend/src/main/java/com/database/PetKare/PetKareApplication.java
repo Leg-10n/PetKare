e248ebca9331aee1ed649389d66c70a29a480634
Kevin <kevin.mon@student.mahidol.edu>
2024-11-27 12:13:11 +0700

    Setup backend and frontend

---
+++ Backend/src/main/java/com/database/PetKare/PetKareApplication.java
@@ -0,0 +1,13 @@
+package com.database.PetKare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetKareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetKareApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/database/PetKare/PetKareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.PetKare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetKareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
